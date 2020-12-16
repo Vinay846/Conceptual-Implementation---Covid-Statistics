dd62e35f1505b5ed6a5c117968cf84b9d145b4a9
Vinay846 <Vinayvin84629@gmail.com>
2020-12-16 18:08:16 +0530

    Solution

--- src/index.js
+++ src/index.js
@@ -8,87 +8,76 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 const { connection } = require('./connector')
 
-app.get("/", async (req, res)=>{
-    res.send(await connection.find({},{
-        "recovered":1,
-        _id:0
-    }));
-})
+
 app.get("/totalRecovered", async (req, res)=>{
-    const recoverData = await connection.find({},{
-        "recovered":1,
-        _id:0
-    });
-    let total = 0;
-    recoverData.forEach(element => {
-        total += element.recovered;
-    });
-    res.send({data: {_id: "total", recovered:total}});
+    const recoverData = await connection.aggregate([
+        {
+            $group: {
+                _id: "total",
+                recovered: { $sum: "$recovered"},
+                // count: { $sum: 1}
+            },
+        },
+    ]);
+    
+    res.send({data: recoverData[0]});
 })
 
 app.get("/totalActive", async (req, res)=>{
-    const infectedRecoveredData = await connection.find({},{
-        "infected":1,
-        "recovered":1,
-        _id:0
-    });
-    let total = 0;
-    infectedRecoveredData.forEach(element => {
-        total += (element.infected - element.recovered);
-    });
-    res.send({data: {_id: "total", active:total}});
+    const activeData = await connection.aggregate([
+        {
+            $group: {
+                _id: "total",
+                recovered: {$sum: "$recovered"},
+                infected: {$sum: "$infected"},
+                // active: {$sum: {$subtract: ["$infected", "$recovered"]}}
+            },
+        },
+    ]);
+    // res.send({data: activeData[0]});
+    res.send({data: {_id: "total", active:activeData[0].infected- activeData[0].recovered}});
 })
 
 app.get("/totalDeath", async (req, res)=>{
-    const totalDeathData = await connection.find({},{
-        "death":1,
-        _id:0
-    });
-    let total = 0;
-    totalDeathData.forEach(element => {
-        total += element.death;
-    });
-    res.send({data: {_id:"total", death:total}});
+    const totalDeathData = await connection.aggregate([
+        {
+            $group: {
+                _id: "total",
+                death: {$sum: "$death"}
+            }
+        }
+    ])
+    res.send({data: totalDeathData[0]});
 })
 
 app.get("/hotspotStates", async (req, res)=>{
-    const DataForRate = await connection.find({},{
-        "infected":1,
-        "recovered":1,
-        "state":1,
-        _id:0
-    });
-    let rate = [];
-    DataForRate.forEach(element => {
-        const val = ((element.infected - element.recovered)/element.infected);
-        if(val > 0.1){
-            rate.push({
-                "state":element.state,
-                "rate": val.toFixed(5)
-            })
+    const DataForRate = await connection.aggregate([
+        {
+            $project: {
+                state: "$state",
+                rate: { $round : [{ $divide: [{$subtract: ["$infected", "$recovered"]}, "$infected"]}, 5] }
+            }, 
+        },
+        {
+            $match: {rate: {$gt: 0.1}}
         }
-    });
-    res.send({data: rate});
+    ]);
+    res.send({data: DataForRate});
 })
 
 app.get("/healthyStates", async (req, res)=>{
-    const mortalityData = await connection.find({},{
-        "infected":1,
-        "death":1,
-        "state":1,
-        _id:0
-    });
-    let mortality = [];
-    mortalityData.forEach(element => {
-        const val = (element.death/element.infected);
-        if(val > 0.005){
-            mortality.push({
-                "state":element.state,
-                "mortality": val.toFixed(5)
-            })
+    const mortalityData = await connection.aggregate([
+        {
+            $project: {
+                state: "$state",
+                mortality: {$round: [{$divide:["$death", "$infected"]},5]}
+            }
+        },
+        {
+            $match: {mortality: {$lt: 0.005}}
         }
-    });
-    res.send({data: mortality});
+    ])
+    res.send({data: mortalityData});
 })
 
 app.listen(port, () => console.log(`App listening on port ${port}!`))
